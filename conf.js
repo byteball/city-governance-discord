97d21e236f67988e82fc5fe8634b5bbc795a7227
Taump <taump@yandex.ru>
2025-06-05 18:03:47 +0200

    add: `conf.js`

---
+++ conf.js
@@ -0,0 +1,24 @@
+"use strict";
+const path = require('path');
+require('dotenv').config({ path: path.dirname(process.mainModule.paths[0]) + '/.env' });
+
+exports.bServeAsHub = false;
+exports.bLight = true;
+
+exports.bNoPassphrase = true;
+
+exports.discord_token = process.env.discord_token;
+exports.discord_channels = [process.env.channel];
+
+exports.hub = process.env.testnet ? 'obyte.org/bb-test' : 'obyte.org/bb';
+
+exports.explorer_base_url = process.env.testnet ? 'https://testnetexplorer.obyte.org/#' : 'https://explorer.obyte.org/#';
+exports.oswap_base_url = process.env.testnet ? 'https://city.obyte.org/' : 'https://city.obyte.org/';
+
+exports.governance_base_AAs = [
+  'JGGFM55N6626QBQWAYBHMBN6A76TVPK5'
+];
+
+exports.token_registry_AA_address = process.env.TOKEN_REGISTRY_AA_ADDRESS;
+
+console.log('finished server conf');
