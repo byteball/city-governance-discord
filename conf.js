9c4d32b87d53f1c4dac0751305ae4aa094ab39e2
Taump <taump@yandex.ru>
2025-06-09 18:18:07 +0200

    feat: update governance_base_AAs to conditionally include mainnet address

--- conf.js
+++ conf.js
@@ -15,8 +15,10 @@ exports.hub = process.env.testnet ? 'obyte.org/bb-test' : 'obyte.org/bb';
 exports.explorer_base_url = process.env.testnet ? 'https://testnetexplorer.obyte.org/#' : 'https://explorer.obyte.org/#';
 exports.oswap_base_url = process.env.testnet ? 'https://city.obyte.org/governance' : 'https://city.obyte.org/governance';
 
-exports.governance_base_AAs = [
+exports.governance_base_AAs = process.env.testnet ? [
   'P5SP4B25G3XNX27MXWZBZUPZ5QXSAW7K', // testnet
+] : [
+  'JGGFM55N6626QBQWAYBHMBN6A76TVPK5' // mainnet
 ];
 
 exports.token_registry_AA_address = process.env.TOKEN_REGISTRY_AA_ADDRESS;
